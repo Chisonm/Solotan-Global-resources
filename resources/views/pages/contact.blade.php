401a4acebe533d06cb882717ff493446daf7ccb1
chisonm <danielchisonum@gmail.com>
2021-10-18 13:42:21 +0100

    added animation

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -3,25 +3,25 @@
 <section class="w3l-contact-breadcrum">
   <div class="breadcrum-bg">
     <div class="container py-5">
-      <p><a href="/">Home</a> &nbsp; / &nbsp; Contact</p>
-      <h2 class="my-3">Contact Us</h2>
-    
-      <p>Nulla dolorem perferendis inventore! posuere cubilia Curae; Nunc non risus in justo convallis feugiat. consectetur adipisicing elit.</p>
+      <p data-aos="fade-down"  data-aos-duration="1000" data-aos-easing="ease-in-out"><a href="/">Home</a> &nbsp; / &nbsp; Contact</p>
+      <h2 data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="my-3">Contact Us</h2>
+
+      <p data-aos="fade-right"  data-aos-duration="2000" data-aos-easing="ease-in-out">Nulla dolorem perferendis inventore! posuere cubilia Curae; Nunc non risus in justo convallis feugiat. consectetur adipisicing elit.</p>
     </div>
   </div>
 </section>
 <!-- contact -->
 <section class="w3l-contacts-12" id="contact">
-    <div class="contact-top pt-5">
+    <div class="pt-5 contact-top">
         <div class="container py-md-3">
-            
+
             <div class="row cont-main-top">
-               
+
                 <!-- contact address -->
-                <div class="contact col-lg-4">
+                <div data-aos="fade-up"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="contact col-lg-4">
                     <div class="cont-subs">
                         <div class="cont-add">
-                            
+
                            <div class="cont-add-rgt">
                             <h4>Address:</h4>
                             <p class="contact-text-sub">PO Box 1212, California, US</p>
@@ -31,7 +31,7 @@
                        </div>
                     </div>
                         <div class="cont-add add-2">
-                            
+
                            <div class="cont-add-rgt">
                             <h4>Email:</h4>
                             <a href="mailto:josephisi777@gmail.com">
@@ -43,7 +43,7 @@
                        </div>
                     </div>
                         <div class="cont-add">
-                           
+
                             <div class="cont-add-rgt">
                                  <h4>Phone:</h4>
                                  <a href="tel:+234-814-215-1114">
@@ -55,31 +55,31 @@
                            </div>
                         </div>
                         <div class="cont-add add-3">
-                           
+
                             <div class="cont-add-rgt">
                                  <h4>Find Us On</h4>
-                                 <div class="main-social-1 mt-2">
+                                 <div class="mt-2 main-social-1">
                                     <a href="https://web.facebook.com/pages/category/Product-Service/Promatex-paint-103214094951471/?_rdc=1&_rdr" target="_blank" class="facebook"><span class="fa fa-facebook"></span></a>
                                     <a href="#twitter" class="twitter"><span class="fa fa-twitter"></span></a>
                                     <a href="#instagram" class="instagram"><span class="fa fa-instagram"></span></a>
                                 </div>
                             </div>
                             <div class="cont-add-lft">
-                               
+
                            </div>
                         </div>
                     </div>
                 </div>
                 <!-- //contact address -->
                  <!-- contact form -->
-                 <div class="contacts12-main col-lg-8 mt-lg-0 mt-5">
+                 <div data-aos="fade-left"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="mt-5 contacts12-main col-lg-8 mt-lg-0">
                  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
                   <script src="assets/js/main.js"></script>
                     <form action="mail.php" method="post" class="main-input">
                       <!-- Starting of successful form message -->
                         <div class="row">
                             <div class="col-12">
-                                <div class="alert alert-success contact__msg" style="display: none" role="alert">
+                                <div data-aos="fade-left"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="alert alert-success contact__msg" style="display: none" role="alert">
                                     Your message was sent successfully.
                                 </div>
                             </div>
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -3,21 +3,21 @@
 <section class="w3l-service-breadcrum">
   <div class="breadcrum-bg">
     <div class="container py-5">
-      <p><a href="/">Home</a> &nbsp; / &nbsp; Services</p>
-      <h2 class="my-3">Our Services</h2>
-      <p>Nulla dolorem perferendis inventore! posuere cubilia Curae; Nunc non risus in justo convallis feugiat. consectetur adipisicing elit.</p>
+      <p data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out"><a href="/">Home</a> &nbsp; / &nbsp; Services</p>
+      <h2 data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="my-3">Our Services</h2>
+      <p data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out">Nulla dolorem perferendis inventore! posuere cubilia Curae; Nunc non risus in justo convallis feugiat. consectetur adipisicing elit.</p>
     </div>
   </div>
 </section>
  <!--  services section -->
 <section class="w3l-index-7" id="service">
-  <div class="features-with-17_sur py-5">
+  <div class="py-5 features-with-17_sur">
     <div class="container py-lg-3">
       <div class="features-with-17-top_sur">
         <div class="row">
-          <div class="col-lg-4 col-md-6 mt-md-0 mt-4">
+          <div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="mt-4 col-lg-4 col-md-6 mt-md-0">
             <div class="features-with-17-right-tp_sur">
-              <div class="features-with-17-left1 mb-3">
+              <div class="mb-3 features-with-17-left1">
                 <span class="fa fa-modx" aria-hidden="true"></span>
               </div>
               <div class="features-with-17-left2">
@@ -26,9 +26,9 @@
               </div>
             </div>
           </div>
-          <div class="col-lg-4 col-md-6 mt-md-0 mt-5">
+          <div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="mt-5 col-lg-4 col-md-6 mt-md-0">
             <div class="features-with-17-right-tp_sur">
-              <div class="features-with-17-left1 mb-3">
+              <div class="mb-3 features-with-17-left1">
                 <span class="fa fa-podcast" aria-hidden="true"></span>
               </div>
               <div class="features-with-17-left2">
@@ -37,9 +37,9 @@
               </div>
             </div>
           </div>
-          <div class="col-lg-4 col-md-6 mt-lg-0 mt-5">
+          <div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="mt-5 col-lg-4 col-md-6 mt-lg-0">
             <div class="features-with-17-right-tp_sur">
-              <div class="features-with-17-left1 mb-3">
+              <div class="mb-3 features-with-17-left1">
                 <span class="fa fa-bar-chart" aria-hidden="true"></span>
               </div>
               <div class="features-with-17-left2">
@@ -48,9 +48,9 @@
               </div>
             </div>
           </div>
-          <div class="col-lg-4 col-md-6 mt-5">
+          <div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="mt-5 col-lg-4 col-md-6">
             <div class="features-with-17-right-tp_sur">
-              <div class="features-with-17-left1 mb-3">
+              <div class="mb-3 features-with-17-left1">
                 <span class="fa fa-anchor" aria-hidden="true"></span>
               </div>
               <div class="features-with-17-left2">
@@ -59,9 +59,9 @@
               </div>
             </div>
           </div>
-          <div class="col-lg-4 col-md-6 mt-5">
+          <div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="mt-5 col-lg-4 col-md-6">
             <div class="features-with-17-right-tp_sur">
-              <div class="features-with-17-left1 mb-3">
+              <div class="mb-3 features-with-17-left1">
                 <span class="fa fa-bath" aria-hidden="true"></span>
               </div>
               <div class="features-with-17-left2">
@@ -70,9 +70,9 @@
               </div>
             </div>
           </div>
-          <div class="col-lg-4 col-md-6 mt-5">
+          <div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="mt-5 col-lg-4 col-md-6">
             <div class="features-with-17-right-tp_sur">
-              <div class="features-with-17-left1 mb-3">
+              <div class="mb-3 features-with-17-left1">
                 <span class="fa fa-ravelry" aria-hidden="true"></span>
               </div>
               <div class="features-with-17-left2">
@@ -90,17 +90,17 @@
  <!--  //services section -->
 <!-- services block1 -->
 <section class="w3-about-3">
-<div class="w3-services py-5">
+<div class="py-5 w3-services">
     <div class="container py-lg-3">
-        
+
         <div class="w3-services-grids py-lg-5">
-          
-            <div class="w3-services-right-grid mt-lg-0 text-center">
-                <h4>Are you planning to buy a House?</h4>
-                <p class="mt-3">Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. On the other hand we denounce with righteous indignation.</p>
-                <a class="btn btn-secondary btn-theme3 mt-3" href="contact.php"> Contact Us</a>
+
+            <div class="text-center w3-services-right-grid mt-lg-0">
+                <h4 data-aos="fade-up"  data-aos-duration="1000" data-aos-easing="ease-in-out">Are you planning to buy a House?</h4>
+                <p data-aos="fade-up"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="mt-3">Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. On the other hand we denounce with righteous indignation.</p>
+                <a data-aos="fade-up"  data-aos-duration="2000" data-aos-easing="ease-in-out" class="mt-3 btn btn-secondary btn-theme3" href="contact.php"> Contact Us</a>
             </div>
-           
+
         </div>
     </div>
 </div>
@@ -108,52 +108,52 @@
 <!-- //services block1 -->
 <!-- services block3 -->
 <section class="w3l-service-3" id="features">
-<div class="w3l-open-block-services py-5">
+<div class="py-5 w3l-open-block-services">
     <div class="container py-lg-3">
-      
-        <div class="heading text-center mx-auto">
-            <h3 class="head">Our Best Features</h3>
-            <p class="my-3 head "> Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+
+        <div class="mx-auto text-center heading">
+            <h3 data-aos="fade-up"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="head">Our Best Features</h3>
+            <p data-aos="fade-up"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="my-3 head "> Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
               Nulla mollis dapibus nunc, ut rhoncus
               turpis sodales quis. Integer sit amet mattis quam.</p>
           </div>
-        <div class="row mt-5 pt-3">
+        <div class="pt-3 mt-5 row">
             <div class="col-lg-4 col-md-6">
                 <div class="card">
-                    
-                    <h4 class="mission">Search Property from Anywhere</h4>
+
+                    <h4 data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="mission">Search Property from Anywhere</h4>
                     <div class="icon-holder">
-                        <span class="fa fa-certificate service-icon" aria-hidden="true"></span>
+                        <span data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="fa fa-certificate service-icon" aria-hidden="true"></span>
                     </div>
-                    <div class="open-description">
+                    <div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="open-description">
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, minima. It is a long established fact that a reader will be distracted by the readable.</p>
-                     
+
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 mt-md-0 mt-5 pt-md-0 pt-3">
+            <div class="pt-3 mt-5 col-lg-4 col-md-6 mt-md-0 pt-md-0">
                 <div class="card">
-                   
-                    <h4 class="mission">Professional & Friendly Agents</h4>
+
+                    <h4 data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="mission">Professional & Friendly Agents</h4>
                     <div class="icon-holder">
-                        <span class="fa fa-user service-icon" aria-hidden="true"></span>
+                        <span data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="fa fa-user service-icon" aria-hidden="true"></span>
                     </div>
                     <div class="open-description">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, minima. It is a long established fact that a reader will be distracted by the readable.</p>
-                        
+                        <p data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, minima. It is a long established fact that a reader will be distracted by the readable.</p>
+
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 mt-lg-0 mt-5 pt-lg-0 pt-3">
+            <div class="pt-3 mt-5 col-lg-4 col-md-6 mt-lg-0 pt-lg-0">
                 <div class="card">
-                   
-                    <h4 class="mission">Buy and Sell Awesome Property</h4>
+
+                    <h4 data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="mission">Buy and Sell Awesome Property</h4>
                     <div class="icon-holder">
-                        <span class="fa fa-home service-icon" aria-hidden="true"></span>
+                        <span data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="fa fa-home service-icon" aria-hidden="true"></span>
                     </div>
                     <div class="open-description">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, minima. It is a long established fact that a reader will be distracted by the readable.</p>
-                       
+                        <p data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, minima. It is a long established fact that a reader will be distracted by the readable.</p>
+
                     </div>
                 </div>
             </div>
