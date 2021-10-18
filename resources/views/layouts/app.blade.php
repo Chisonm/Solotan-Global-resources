70c2bf02528d7d756dd1ae1876112f499f98c26d
chisonm <danielchisonum@gmail.com>
2021-10-18 13:15:23 +0100

    added animation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,18 +9,24 @@
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="{{asset('assets/js/main.js')}}"></script>
 
-    
+
      <!-- web fonts -->
     <link href="//fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
     <link href="//fonts.googleapis.com/css?family=Hind&display=swap" rel="stylesheet">
     <!-- //web fonts -->
     <!-- Template CSS -->
     <link rel="stylesheet" href="{{asset('assets/css/style-starter.css')}}">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
   </head>
 <body>
 @include('layouts.includes.header')
 @yield('content')
 @include('layouts.includes.footer')
 @include('layouts.scripts.scripts')
+
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script>
+  AOS.init();
+</script>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,17 @@
 <section class="w3l-main-slider" id="home">
   <!-- main-slider -->
   <div class="companies20-content">
-    
+
     <div class="owl-one owl-carousel owl-theme">
       <div class="item">
         <li>
           <div class="slider-info banner-view bg bg2" data-selector=".bg.bg2">
             <div class="banner-info">
               <div class="container">
-                <div class="banner-info-bg mr-auto">
+                <div class="mr-auto banner-info-bg">
                   <h5> We are architects, planners &amp; designers.</h5>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, velit recusandae eum necessitatibus blanditiis porro cum</p>
-                  <a class="btn btn-secondary btn-theme2 mt-md-5 mt-4" href="services.php"> Our Services</a>
+                  <a class="mt-4 btn btn-secondary btn-theme2 mt-md-5" href="services.php"> Our Services</a>
                 </div>
               </div>
             </div>
@@ -22,13 +22,13 @@
       </div>
       <div class="item">
         <li>
-          <div class="slider-info  banner-view banner-top1 bg bg2" data-selector=".bg.bg2">
+          <div class="slider-info banner-view banner-top1 bg bg2" data-selector=".bg.bg2">
             <div class="banner-info">
               <div class="container">
-                <div class="banner-info-bg mr-auto">
+                <div class="mr-auto banner-info-bg">
                   <h5>We Create Beautiful Home Exteriors.</h5>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, velit recusandae eum necessitatibus blanditiis porro cum</p>
-                  <a class="btn btn-secondary btn-theme2 mt-md-5 mt-4" href="contact.php"> Contact Us</a>
+                  <a class="mt-4 btn btn-secondary btn-theme2 mt-md-5" href="contact.php"> Contact Us</a>
                 </div>
               </div>
             </div>
@@ -40,10 +40,10 @@
           <div class="slider-info banner-view banner-top2 bg bg2" data-selector=".bg.bg2">
             <div class="banner-info">
               <div class="container">
-                <div class="banner-info-bg mr-auto">
+                <div class="mr-auto banner-info-bg">
                   <h5>Exceptional Designing For Exceptional Spaces</h5>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, velit recusandae eum necessitatibus blanditiis porro cum</p>
-                  <a class="btn btn-secondary btn-theme2 mt-md-5 mt-4" href="about.php"> About Us</a>
+                  <a class="mt-4 btn btn-secondary btn-theme2 mt-md-5" href="about.php"> About Us</a>
                 </div>
               </div>
             </div>
@@ -55,10 +55,10 @@
           <div class="slider-info banner-view banner-top3 bg bg2" data-selector=".bg.bg2">
             <div class="banner-info">
               <div class="container">
-                <div class="banner-info-bg mr-auto">
+                <div class="mr-auto banner-info-bg">
                   <h5>Giving Your Home a New Style Every Style</h5>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, velit recusandae eum necessitatibus blanditiis porro cum</p>
-                  <a class="btn btn-secondary btn-theme2 mt-md-5 mt-4" href="services.php">Read More</a>
+                  <a class="mt-4 btn btn-secondary btn-theme2 mt-md-5" href="services.php">Read More</a>
                 </div>
               </div>
             </div>
@@ -110,22 +110,22 @@
 </section>
  <!--  services section -->
 <section class="w3l-index6" id="about">
-  <div class="features-with-17_sur py-5">
+  <div class="py-5 features-with-17_sur">
     <div class="container py-lg-5">
-      <div class="heading text-center mx-auto mb-5">
+      <div class="mx-auto mb-5 text-center heading">
         <h3 class="head">Build your base</h3>
         <p class="my-3 head"> Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
         Nulla mollis dapibus nunc, ut rhoncus
         turpis sodales quis. Integer sit amet mattis quam.</p>
       </div>
-      <div class="features-with-17-top_sur mt-5 pt-3">
+      <div class="pt-3 mt-5 features-with-17-top_sur">
         <div class="row">
-          <div class="col-lg-4 col-md-6 mt-md-0 mt-4">
+          <div data-aos="fade-up"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="mt-4 col-lg-4 col-md-6 mt-md-0">
             <div class="features-with-17-right-tp_sur">
-              <div class="features-with-17-left1 mb-3">
+              <div class="mb-3 features-with-17-left1">
                 <span class="fa fa-paint-brush" aria-hidden="true"></span>
               </div>
-              <div class="features-with-17-left2">
+              <div  class="features-with-17-left2">
                 <h6><a href="#url">Conceptual Architecture</a></h6>
                 <p> Lorem Ipsum is simply dummy text of the printing and industry.
                   Lorem Ipsum has the industry's standard dummy text ever since the 1500s,</p>
@@ -133,9 +133,9 @@
               </div>
             </div>
           </div>
-          <div class="col-lg-4 col-md-6 mt-md-0 mt-5">
+          <div data-aos="fade-up"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="mt-5 col-lg-4 col-md-6 mt-md-0">
             <div class="features-with-17-right-tp_sur">
-              <div class="features-with-17-left1 mb-3">
+              <div class="mb-3 features-with-17-left1">
                 <span class="fa fa-ils" aria-hidden="true"></span>
               </div>
               <div class="features-with-17-left2">
@@ -146,9 +146,9 @@
               </div>
             </div>
           </div>
-          <div class="col-lg-4 col-md-6 mt-lg-0 mt-5">
+          <div data-aos="fade-up"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="mt-5 col-lg-4 col-md-6 mt-lg-0">
             <div class="features-with-17-right-tp_sur">
-              <div class="features-with-17-left1 mb-3">
+              <div class="mb-3 features-with-17-left1">
                 <span class="fa fa-building" aria-hidden="true"></span>
               </div>
               <div class="features-with-17-left2">
@@ -166,19 +166,19 @@
 </section>
  <!--  //services section -->
  <!--  About section -->
- <div class="w3l-about1 py-5" id="about">
+ <div class="py-5 w3l-about1" id="about">
   <div class="container py-lg-3">
-   
+
     <div class="aboutgrids row">
-      <div class="col-lg-6 aboutgrid2">
-        <img src="assets/images/a2.jpg" alt="about image" class="img-fluid" />
+      <div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="col-lg-6 aboutgrid2">
+        <img src="{{ asset('assets/images/a2.jpg') }}" alt="about image" class="img-fluid" />
       </div>
-      <div class="col-lg-6 aboutgrid1 mt-lg-0 mt-4 pl-lg-5">
+      <div data-aos="fade-left"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="mt-4 col-lg-6 aboutgrid1 mt-lg-0 pl-lg-5">
         <h4>We never stop to create new ideas</h4>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, velit recusandae eum necessitatibus blanditiis porro cum, facere qui impedit dolor doloribus quis reiciendis ullam repellendus.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, velit recusandae eum </p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, velit recusandae eum necessitatibus blanditiis porro cum, facere qui impedit dolor doloribus quis reiciendis ullam repellendus.</p>
 
-        <a class="btn btn-secondary btn-theme2" href="about.php"> Learn More</a>
+        <a data-aos="fade-up"  data-aos-duration="2000" data-aos-easing="ease-in-out" class="btn btn-secondary btn-theme2" href="about.php"> Learn More</a>
       </div>
     </div>
   </div>
@@ -186,40 +186,40 @@
  <!--  //About section -->
 <!-- specifications -->
 <section class="w3l-specifications-9">
-    <div class="main-w3 py-5" id="stats">
+    <div class="py-5 main-w3" id="stats">
         <div class="container py-md-3">
-            
+
            <div class="main-cont-wthree-fea row ">
                 <div class="grids-speci1 col-lg-3 col-6">
-                    <div class="spec-2 text-center">
+                    <div class="text-center spec-2">
                      <span class="fa fa-hourglass"></span>
                     <h3 class="title-spe">7242</h3>
                     <p>Hours of Works</p>
                 </div>
                 </div>
                 <div class="grids-speci1 midd-eff-spe col-lg-3 col-6">
-                    <div class="spec-2 text-center">
+                    <div class="text-center spec-2">
                         <span class="fa fa-folder"></span>
                     <h3 class="title-spe">380</h3>
                     <p>Projects Done</p>
                     </div>
                 </div>
-                <div class="grids-speci1 las-but col-lg-3 col-6  mt-lg-0 mt-4">
-                    <div class="spec-2 text-center">
+                <div class="mt-4 grids-speci1 las-but col-lg-3 col-6 mt-lg-0">
+                    <div class="text-center spec-2">
                         <span class="fa fa-smile-o"></span>
                     <h3 class="title-spe">220</h3>
                     <p>Satisfied Customers</p>
                     </div>
                 </div>
-                <div class="grids-speci1 las-t col-lg-3 col-6  mt-lg-0 mt-4">
-                    <div class="spec-2 text-center">
+                <div class="mt-4 grids-speci1 las-t col-lg-3 col-6 mt-lg-0">
+                    <div class="text-center spec-2">
                         <span class="fa fa-trophy"></span>
                         <h3 class="title-spe">24 </h3>
                         <p>Awards Winning</p>
                         </div>
                     </div>
             </div>
-            
+
         </div>
     </div>
     <!-- //specifications -->
@@ -228,75 +228,75 @@
     <!-- Products4 block -->
     <div id="products4-block" class="text-center">
         <div class="container">
-            <div class="heading text-center mx-auto mb-5">
+            <div class="mx-auto mb-5 text-center heading">
                 <h3 class="head">Projects</h3>
                 <p class="my-3 head"> Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
                 Nulla mollis dapibus nunc, ut rhoncus
                 turpis sodales quis. Integer sit amet mattis quam.</p>
               </div>
               <input id="tab1" type="radio" name="tabs" checked>
-              
 
-            <section id="content1" class="tab-content text-left">
+
+            <section id="content1" class="text-left tab-content">
                 <div class="d-grid grid-col-3">
-                    <div class="product">
+                    <div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="product">
                         <a href="assets/images/WhatsApp Image 2020-12-17 at 10.04.35 AM (1).jpeg" data-lightbox="example-set"
                             data-title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam architecto, ex veritatis">
                             <figure>
-                                <img src="assets/images/WhatsApp Image 2020-12-17 at 10.04.35 AM (1).jpeg" class="img-responsive" alt="" />
+                                <img src="{{ asset('assets/images/WhatsApp Image 2020-12-17 at 10.04.35 AM (1).jpeg') }}" class="img-responsive" alt="" />
                             </figure>
                         </a>
-                        
+
                     </div>
-                    <div class="product">
+                    <div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="product">
                         <a href="assets/images/WhatsApp Image 2020-12-17 at 10.06.41 AM.jpeg" data-lightbox="example-set"
                             data-title="Promatex Epoxy finish paint">
                             <figure>
                                 <img src="assets/images/WhatsApp Image 2020-12-17 at 10.06.41 AM.jpeg" class="img-responsive" alt="" />
                             </figure>
                         </a>
-                        
+
                     </div>
-                    <div class="product">
+                    <div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="product">
                         <a href="assets/images/WhatsApp Image 2020-12-17 at 10.07.53 AM.jpeg" data-lightbox="example-set"
                             data-title="Matt and Emulsion Paint">
                             <figure>
                                 <img src="assets/images/WhatsApp Image 2020-12-17 at 10.07.53 AM.jpeg" class="img-responsive" alt="" />
                             </figure>
                         </a>
-                        
+
                     </div>
-                    <div class="product">
+                    <div data-aos="fade-left"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="product">
                         <a href="assets/images/WhatsApp Image 2020-12-17 at 10.06.43 AM (1).jpeg" data-lightbox="example-set"
                             data-title="Promatex Epoxy finish paint">
                             <figure>
                                 <img src="assets/images/WhatsApp Image 2020-12-17 at 10.06.43 AM (1).jpeg" class="img-responsive" alt="" />
                             </figure>
                         </a>
-                        
+
                     </div>
-                    <div class="product">
+                    <div data-aos="fade-left"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="product">
                       <a href="assets/images/WhatsApp Image 2020-12-17 at 10.07.51 AM.jpeg" data-lightbox="example-set"
                           >
                           <figure>
                               <img src="assets/images/WhatsApp Image 2020-12-17 at 10.07.51 AM.jpeg" class="img-responsive" alt="" />
                           </figure>
                       </a>
-                      
+
                   </div>
 
-                    
-                    <div class="product">
+
+                    <div data-aos="fade-left"  data-aos-duration="1000" data-aos-easing="ease-in-out" class="product">
                         <a href="assets/images/WhatsApp Image 2020-12-17 at 5.32.41 PM.jpeg" data-lightbox="example-set"
                             >
                             <figure>
                                 <img src="assets/images/WhatsApp Image 2020-12-17 at 5.32.41 PM.jpeg" class="img-responsive" alt="" />
                             </figure>
                         </a>
-                        
+
                     </div>
-                    
-                    
+
+
                 </div>
             </section>
         </div>
@@ -322,7 +322,7 @@
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
     integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
   </script>
-  
+
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
     integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
   </script>
